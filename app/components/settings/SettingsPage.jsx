a948387bf692036e17860bc9d013654e3a0530c8
ariel batista <arielbatista_7@hotmail.com>
2019-10-14 14:56:45 -0400

    fix overflow on setting page

--- app/components/settings/SettingsPage.jsx
+++ app/components/settings/SettingsPage.jsx
@@ -17,82 +17,85 @@ const SettingsPage = () => {
     const height = useSelector(state => state.size.viewHeight)
     const interfaceLanguage = useSelector(state => state.settings.interfaceLanguage)
 
-    return <div className="setting-container" style={{ height }}>
-        <div className="row">
-            <div className="col s12">
-                {interfaceLanguage.username}
+    return <div className="setting-page"  style={{ height }}>
+        <div className="setting-container" style={{ height }}>
+            <div className="row">
+                <div className="col s12">
+                    {interfaceLanguage.username}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                <TextBox name="username"
-                    onKeyUp={(e) => dispatch(handleSettingsChange(e))}
-                    defaultValue={username} />
+            <div className="row">
+                <div className="col s12">
+                    <TextBox name="username"
+                        onKeyUp={(e) => dispatch(handleSettingsChange(e))}
+                        defaultValue={username} />
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                {interfaceLanguage.interface}
+            <div className="row">
+                <div className="col s12">
+                    {interfaceLanguage.interface}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={themelightSelected}
-                    name="theme" value={themeLight} /> {interfaceLanguage.light}
+            <div className="row">
+                <div className="col s12">
+                    <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={themelightSelected}
+                        name="theme" value={themeLight} /> {interfaceLanguage.light}
 
-                <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={!themelightSelected}
-                    name="theme" value={themeDark} className="margin-left-10" /> {interfaceLanguage.dark}
+                    <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={!themelightSelected}
+                        name="theme" value={themeDark} className="margin-left-10" /> {interfaceLanguage.dark}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-            {interfaceLanguage.clock}
+            <div className="row">
+                <div className="col s12">
+                    {interfaceLanguage.clock}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" name="clock"
-                    value={12} checked={!_24Clock} /> {interfaceLanguage._12}
-                 <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" name="clock"
-                    value={24} checked={_24Clock} className="margin-left-10" /> {interfaceLanguage._24}
+            <div className="row">
+                <div className="col s12">
+                    <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" name="clock"
+                        value={12} checked={!_24Clock} /> {interfaceLanguage._12}
+                    <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" name="clock"
+                        value={24} checked={_24Clock} className="margin-left-10" /> {interfaceLanguage._24}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-            {interfaceLanguage.ctrlEnter}
+            <div className="row">
+                <div className="col s12">
+                    {interfaceLanguage.ctrlEnter}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={sendCTRLandEnter}
-                    name="sendCTRLandEnter" value={true} /> {interfaceLanguage.on}
-                 <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={!sendCTRLandEnter}
-                    name="sendCTRLandEnter" value={false} className="margin-left-10" /> {interfaceLanguage.off}
+            <div className="row">
+                <div className="col s12">
+                    <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={sendCTRLandEnter}
+                        name="sendCTRLandEnter" value={true} /> {interfaceLanguage.on}
+                    <input onChange={(e) => dispatch(handleSettingsChange(e))} type="radio" checked={!sendCTRLandEnter}
+                        name="sendCTRLandEnter" value={false} className="margin-left-10" /> {interfaceLanguage.off}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-            {interfaceLanguage.language}
+            <div className="row">
+                <div className="col s12">
+                    {interfaceLanguage.language}
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                <Select name="language"
-                    data={languageList}
-                    labelColumn="name"
-                    valueColumn="value"
-                    selectedValue={language}
-                    onChange={(e) => dispatch(handleSettingsChange(e))}
-                />
+            <div className="row">
+                <div className="col s12">
+                    <Select name="language"
+                        data={languageList}
+                        labelColumn="name"
+                        valueColumn="value"
+                        selectedValue={language}
+                        onChange={(e) => dispatch(handleSettingsChange(e))}
+                    />
+                </div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col s12">
-                <button onClick={() => dispatch(clearSettings())} id="btn-reset"
-                    className="btn"> {interfaceLanguage.btnReset}</button>
+            <div className="row">
+                <div className="col s12">
+                    <button onClick={() => dispatch(clearSettings())} id="btn-reset"
+                        className="btn"> {interfaceLanguage.btnReset}</button>
+                </div>
             </div>
         </div>
     </div>
+
 }
 
 export default SettingsPage
